6b9806c817db18623ab4c3dfe6decc6999a5e0ea
mrale <mrale@DESKTOP-HHS76V9>
2024-11-27 00:19:16 +0300

    Добавьте файлы проекта.

---
+++ Task1/LinearEquation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    struct LinearEquation
+    {
+        double k;
+        double x;
+        double b;
+
+
+
+        public LinearEquation(double k, double x, double b)
+        { 
+        this.k = k;
+        this.x = x;
+        this.b = b;
+
+        }
+
+        public string Root()
+        {
+            if (k == 0)
+                return "Значение K не может быть меньше нуля"; 
+        double x = (-b) / k;
+            return $"X равен {x}";
+        }
+           
+
+    }       
+
+
+
+    
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 0;
+            Console.WriteLine("Введите значение коэффициента K");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение коэффициента B");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            LinearEquation linearEquation = new LinearEquation(k,x,b);
+            Console.WriteLine(linearEquation.Root());
+            Console.ReadKey();
+
+        }
+    }
+}
